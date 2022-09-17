0cb0e3edea67b2eee324967c3bf2f638600d62a5
Анияр Дурмагамбетова <durmagambetovaa4@gmail.com>
2022-09-17 22:57:33 +0600

    c# backend: Attendance Check, Medical Check, Trip Start and Finish

---
+++ backend/c#/SilkwayTransitWork/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SilkwayTransitWork.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string Password { get; set; }
+    }
+}
